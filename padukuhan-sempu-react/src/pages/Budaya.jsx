c852481f023d70b779fb0826a81c007fba6fff63
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2023-07-01 17:08:39 +0700

    add navbar

--- padukuhan-sempu-react/src/pages/Budaya.jsx
+++ padukuhan-sempu-react/src/pages/Budaya.jsx
@@ -1,5 +1,22 @@
+import React from "react";
+
 const Budaya = () => {
-  return <div>Budaya</div>;
+  return (
+    <>
+      <header className="bg-white shadow">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            BUDAYA
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          {/* KONTEN */}
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Budaya;
