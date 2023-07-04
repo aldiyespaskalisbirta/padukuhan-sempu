fc3043e5b7f3d0bff995c89905efa4b137e7a31d
Hallo Bap! <sandyyuan53@gmail.com>
2023-07-04 17:18:34 +0700

    navbar transisi

--- padukuhan-sempu-react/src/components/Navbar.jsx
+++ padukuhan-sempu-react/src/components/Navbar.jsx
@@ -1,36 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { UserIcon } from "@heroicons/react/24/outline";
 import { NavLink } from "react-router-dom";
 
 const navigation = [
   { name: "Home", to: "/" },
   { name: "Budaya", to: "/budaya" },
-  { name: "Gallery Foto", to: "/gallery/images" },
-  { name: "Kontak", to: "/kontak" },
+  { name: "Gallery Foto", to: "gallery" },
+  { name: "Kontak", to: "kontak" },
 ];
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-
 const Navbar = () => {
+  const [hoveredLink, setHoveredLink] = useState(null);
+
+  const handleLinkHover = (index) => {
+    setHoveredLink(index);
+  };
+
+  const handleLinkLeave = () => {
+    setHoveredLink(null);
+  };
+
   return (
     <nav className="navbar flex justify-between items-center h-14 opacity-80 px-24 bg-green-800 fixed top-0 left-0 right-0 z-50">
       <div className="logo text-white font-bold text-2xl">SEMPU I</div>
       <div className="flex flex-row gap-3">
         <div className="flex justify-center items-center">
-          {navigation.map((item) => (
+          {navigation.map((item, index) => (
             <NavLink
               key={item.name}
               to={item.to}
-              className={({ isActive }) =>
-                classNames(
-                  isActive
-                    ? "text-semibold text-white"
-                    : "text-gray-300  hover:text-white",
-                  "rounded-md px-3 py-2 text-sm font-medium"
-                )
-              }
+              className={`${
+                hoveredLink === index ? "text-white" : "text-gray-300"
+              } hover:text-white rounded-md px-3 py-2 text-sm font-medium transition-colors duration-500`}
+              onMouseEnter={() => handleLinkHover(index)}
+              onMouseLeave={handleLinkLeave}
             >
               {item.name}
             </NavLink>
